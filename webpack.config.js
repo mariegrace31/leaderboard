fbb098600f8c6e4c814599e084076a3e4b491d3e
mariegrace31 <mariegracebmg@gmail.com>
2023-06-30 18:25:11 +0200

    Resolved the linters issues

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,10 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.js$/,
+        exclude: '/node_modules',
+      },
     ],
   },
   plugins: [
